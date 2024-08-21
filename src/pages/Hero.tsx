89fad544f87bb4957e1df3e64a039f9a2590e1ac
germanp007 <pintogerman281@gmail.com>
2024-08-21 14:06:45 -0300

    refactoring

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+// import image from "../assets/images/background-desktop.jpg";
 const Hero: React.FC = () => {
-  return <section className="hero-section h-screen w-screen"></section>;
+  return (
+    <section className="hero-section h-[800px] w-screen" id="hero-section">
+      {/* <img src={image} alt="hero-background" /> */}
+    </section>
+  );
 };
 
 export default Hero;
