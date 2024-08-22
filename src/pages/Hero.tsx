4c5ec2c2a9501ca918ca3388b855ca51fc28dd5c
germanp007 <pintogerman281@gmail.com>
2024-08-22 18:18:54 -0300

    refactoring

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -8,12 +8,14 @@ const Hero: React.FC = () => {
       className="hero-section h-screen md:h-[750px] w-screen grid place-items-end lg:place-items-center"
       id="hero-section"
     >
-      <div className="grid gap-4 w-full h-auto p-4 lg:ml-20 lg:gap-8">
+      <div className="grid gap-4 w-full h-auto p-4 lg:ml-48 lg:gap-8">
         <TitleMain className="h-auto">La Solución que buscas</TitleMain>
         <Paragraph className="text-white text-lg tracking-[3.6px] font-light h-auto lg:text-2xl">
           En un solo Lugar
         </Paragraph>
-        <Button>Ver Servicios</Button>
+        <Button className="hover:bg-primary hover:text-white  transition-all duration-300">
+          Ver Servicios
+        </Button>
       </div>
     </section>
   );
