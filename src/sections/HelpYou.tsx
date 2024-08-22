13a4aed46a49528ade800c0955d72f8b4122421a
germanp007 <pintogerman281@gmail.com>
2024-08-22 18:59:11 -0300

    haciendo responsive en help section

--- src/sections/HelpYou.tsx
+++ src/sections/HelpYou.tsx
@@ -4,17 +4,18 @@ import { Paragraph } from "../components/generics/paragraph/Paragraph";
 import meet from "../assets/images/Ellipse.jpg";
 const HelpYou: React.FC = () => {
   return (
-    <section className="w-full h-[60vh] bg-white flex flex-col justify-center items-center gap-8">
+    <section className="w-full h-[50vh] bg-white flex flex-col justify-center items-center gap-8 px-4">
       <Titles className="text-left w-full">Podemos ayudarte</Titles>
       <div className="w-full flex ">
-        <Paragraph className="flex-1">
+        <Paragraph className=" w-[174px]">
           En Tel & Tech contamos con el mejor equipo capacitado para ayudarte a
           que CREES eso que necesitas para hacer crecer tu negocio.
         </Paragraph>
-        <div className="flex-1 relative">
-          <div className="w-[113px] h-[107px] bg-[#A7C6E6] rounded-full absolute z-1"></div>
-
-          <img src={meet} alt="meet" className="w-[113px] h-[107px] " />
+        <div className="flex-1 relative ">
+          <div className="w-[113px] h-[107px] bg-[#A7C6E6] rounded-full absolute top-2 left-1/2 transform -translate-x-1/2"></div>
+          <div className="absolute z-10 w-[113px] h-[107px] rounded-full overflow-hidden top-4 left-[45%] transform -translate-x-1/2">
+            <img src={meet} alt="meet" className="w-[113px] h-[107px] " />
+          </div>
         </div>
       </div>
     </section>
