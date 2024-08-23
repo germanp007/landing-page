c99a5f5efc12443bdfe5dbd96415f9e23806f514
germanp007 <pintogerman281@gmail.com>
2024-08-23 14:46:10 -0300

    fixing and debugging

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -8,7 +8,7 @@ const Hero: React.FC = () => {
       className="hero-section h-screen md:h-[750px] w-screen grid place-items-end lg:place-items-center"
       id="hero-section"
     >
-      <div className="grid gap-4 w-full h-auto p-4 lg:ml-48 lg:gap-8">
+      <div className="grid gap-4 p-4 w-full h-auto md:px-16 lg:gap-8 ">
         <TitleMain className="h-auto">La Solución que buscas</TitleMain>
         <Paragraph className="text-white text-lg tracking-[3.6px] font-light h-auto lg:text-2xl">
           En un solo Lugar
