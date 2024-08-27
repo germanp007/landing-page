9ce0b6236540ffb6d37b746b23c035f34058807b
germanp007 <pintogerman281@gmail.com>
2024-08-27 14:45:22 -0300

    Creando componente Card

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -5,7 +5,9 @@ const Services = () => {
   return (
     <>
       <section className="bg-sectionBg h-[60vh] flex flex-col justify-evenly items-center ">
-        <Titles className="text-white">Nuestros Servicios</Titles>
+        <Titles className="text-white w-full text-center">
+          Nuestros Servicios
+        </Titles>
         <div className="w-screen flex justify-center items-center overflow-x-auto space-x-4 snap-x snap-mandatory px-6 scroll-container">
           <div className="flex gap-8">
             <div className="snap-center shrink-0">
---
+++ src/utils/constants.ts
@@ -0,0 +1,41 @@
+import webApp from '../assets/icons/icon_01.svg'
+
+
+
+export const SERVICES_DATA = [
+    {
+        id: 1,
+        title: "Desarrollo de App de Escritorio",
+        description: "This is a description of service 1",
+        icon: 
+    }, 
+    {
+        id: 2,
+        title: "Desarrollo de Apps Móviles",
+        description: "This is a description of service 1",
+        icon: 
+    }, 
+    {
+        id: 3,
+        title: "E-Comerce",
+        description: "This is a description of service 1",
+        icon: 
+    }, {
+        id: 4,
+        title: "Asesorías tecnológicas",
+        description: "This is a description of service 1",
+        icon: 
+    },
+     {
+        id: 5,
+        title: "Desarollo de contenido web",
+        description: "This is a description of service 1",
+        icon: 
+     },
+      {
+        id: 5,
+        title: "Compra y venta de productos digitales",
+        description: "This is a description of service 1",
+        icon: 
+      }
+    ];
