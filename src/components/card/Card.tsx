4e83fdfc7f4345aff042c98a12313a332e2a2572
germanp007 <pintogerman281@gmail.com>
2024-08-28 13:24:43 -0300

    creando card component

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,7 +1,20 @@
+import { webApp } from "../../assets/icons";
+import { Paragraph } from "../generics/paragraph/Paragraph";
 import "./card.css";
 
 const Card = () => {
-  return <div className="card"></div>;
+  return (
+    <div className="card flex flex-col">
+      <div className="flex-1">
+        <img src={webApp} alt="Web App" />
+      </div>
+      <div className="flex-1">
+        <Paragraph className="text-[#111827] text-center text-base font-semibold ">
+          Desarollo de App de escritorio
+        </Paragraph>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
