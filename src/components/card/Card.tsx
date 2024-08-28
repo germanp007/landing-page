7752775eb223fde303e86337271a0c70f9ba88d6
germanp007 <pintogerman281@gmail.com>
2024-08-28 19:38:35 -0300

    agregando estilos

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,17 +1,36 @@
-import { webApp } from "../../assets/icons";
-import { Paragraph } from "../generics/paragraph/Paragraph";
+import React from "react";
+import { Paragraph, ParagraphCard } from "../generics/paragraph/Paragraph";
 import "./card.css";
 
-const Card = () => {
+interface ElementProps {
+  id: number;
+  title: string;
+  description: string;
+  icon: string;
+}
+
+interface CartProps {
+  element: ElementProps;
+}
+const Card: React.FC<CartProps> = ({ element }) => {
   return (
     <div className="card flex flex-col p-4">
       <div className="flex-1  grid place-content-center">
-        <img src={webApp} alt="Web App" className="w-[70%] m-auto" />
+        <div className="w-full h-full overflow-hidden">
+          <img
+            src={element.icon}
+            alt="Web App"
+            className={`${
+              element.title === "E-Comerce" ? "w-[100%] " : "w-[80%]"
+            } m-auto`}
+          />
+        </div>
       </div>
       <div className="flex-1 grid place-content-center">
         <Paragraph className="text-[#111827] text-center text-base font-semibold ">
-          Desarollo de App de escritorio
+          {element.title}
         </Paragraph>
+        <ParagraphCard>{element.description}</ParagraphCard>
       </div>
     </div>
   );
--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -3,33 +3,21 @@
 
 import Card from "../components/card/Card";
 import { Titles } from "../components/generics/titles/Titles";
+import { SERVICES_DATA } from "../utils/constants";
 
 const Services = () => {
   return (
     <>
-      <section className="bg-sectionBg h-[60vh] w-screen flex flex-col overflow-x-hidden space-y-4 py-4 justify-evenly ">
+      <section className="bg-sectionBg h-[60vh] w-screen flex flex-col overflow-x-hidden space-y-4 py-4 justify-evenly md:h-screen">
         <Titles className="text-white w-full text-center">
           Nuestros Servicios
         </Titles>
         <div className="flex overflow-x-auto space-x-4 snap-x snap-mandatory scroll-container gap-4 px-6">
-          <div className="snap-center shrink-0">
-            <Card />
-          </div>
-          <div className="snap-center shrink-0">
-            <Card />
-          </div>
-          <div className="snap-center shrink-0">
-            <Card />
-          </div>
-          <div className="snap-center shrink-0">
-            <Card />
-          </div>
-          <div className="snap-center shrink-0">
-            <Card />
-          </div>
-          <div className="snap-center shrink-0">
-            <Card />
-          </div>
+          {SERVICES_DATA.map((ele) => (
+            <div className="snap-center shrink-0" key={ele.id}>
+              <Card element={ele} />
+            </div>
+          ))}
         </div>
       </section>
     </>
