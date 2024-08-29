5e9bce4b02aca4e37f2164141a3a02df83d844ec
germanp007 <pintogerman281@gmail.com>
2024-08-29 13:25:44 -0300

    refactoring y mejoras en el layout

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -12,42 +12,42 @@ export const SERVICES_DATA = [
     id: 1,
     title: "Desarrollo de App de Escritorio",
     description:
-      "Anima’s Landing Page UI Kit has become a staple in my design toolkit. This kit has everything I need to get the job done quickly and efficiently.",
+      "Creamos aplicaciones robustas y personalizadas para Windows y macOS, diseñadas para optimizar tus procesos y mejorar la productividad. ",
     icon: webApp,
   },
   {
     id: 2,
     title: "Desarrollo de Apps Móviles",
     description:
-      "Anima’s Landing Page UI Kit has become a staple in my design toolkit. This kit has everything I need to get the job done quickly and efficiently.",
+      "Ofrecemos servicios completos de desarrollo de apps para iOS y Android, desde la conceptualización y el diseño hasta la implementación y el mantenimiento.",
     icon: appMovile,
   },
   {
     id: 3,
     title: "E-Comerce",
     description:
-      "Anima’s Landing Page UI Kit has become a staple in my design toolkit. This kit has everything I need to get the job done quickly and efficiently.",
+      "Impulsa tu negocio al mundo digital con nuestras soluciones de ecommerce. Creamos tiendas en línea personalizadas y optimizadas para ofrecer una experiencia de compra fluida y segura.",
     icon: ecomerce,
   },
   {
     id: 4,
     title: "Asesorías tecnológicas",
     description:
-      "Anima’s Landing Page UI Kit has become a staple in my design toolkit. This kit has everything I need to get the job done quickly and efficiently.",
+      "Ofrecemos soluciones personalizadas para abordar tus desafíos tecnológicos, desde la implementación de nuevas herramientas hasta la mejora de sistemas existentes. ",
     icon: asesorias,
   },
   {
     id: 5,
     title: "Desarollo de contenido web",
     description:
-      "Anima’s Landing Page UI Kit has become a staple in my design toolkit. This kit has everything I need to get the job done quickly and efficiently.",
+      "Atrae y retén a tus usuarios con contenido web de alta calidad y relevancia. Ofrecemos servicios integrales de desarrollo de contenido, desde la creación de textos optimizados para SEO hasta la producción de imágenes y videos atractivos. ",
     icon: contenidoWeb,
   },
   {
     id: 5,
     title: "Compra y venta de productos digitales",
     description:
-      "Anima’s Landing Page UI Kit has become a staple in my design toolkit. This kit has everything I need to get the job done quickly and efficiently.",
+      "Explora el mundo de los productos digitales con nuestras soluciones de compra y venta. Ofrecemos una plataforma segura y eficiente para adquirir y comercializar bienes electrónicos como eBooks, cursos en línea, software, plantillas y más.",
     icon: venta_productos,
   },
 ];
