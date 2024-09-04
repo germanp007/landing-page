8ba297be1b1677d829fda65a31a9afffc3494a7c
germanp007 <pintogerman281@gmail.com>
2024-09-04 14:07:05 -0300

    dando estilos y refactoring

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,16 @@
 import "./footer.css";
+import logo_gris from "../../assets/logos/logo_gris.svg";
+import { TitleLogo } from "../generics/titles/Titles";
+
 export const Footer = () => {
   return (
     <footer className="footer grid grid-cols-2">
-      <div className=""></div>
+      <div className="flex flex-col justify-center items-center gap-4">
+        <img src={logo_gris} alt="logo" className="size-[50px]" />
+        <TitleLogo className="text-[12px]  font-normal">
+          Tel & Tech Services
+        </TitleLogo>
+      </div>
       <div className=""></div>
     </footer>
   );
