f6a50c5457db64a0442a7d35543fc89fa2b2d28c
germanp007 <pintogerman281@gmail.com>
2024-10-01 16:00:14 -0300

    agregando idiomas

---
+++ src/locales/languages/es.ts
@@ -0,0 +1,20 @@
+export const ES = {
+  navbar: {
+    about: "Sobre Nosotros",
+    tech: "Tecnologías",
+    contact: "Contactanos",
+  },
+  hero: {
+    title: "La Solucion que buscas",
+    subtitle: "en un solo lugar",
+  },
+  aboutUs: {
+    title: "Podemos Ayudarte",
+    paragraph:
+      "En Tel & Tech contamos con el mejor equipo capacitado para ayudarte a que CREES eso que necesitas para hacer crecer tu negocio. Con un equipo apasionado y en constante aprendizaje, garantizamos soluciones tecnológicas a la vanguardia.",
+  },
+  services: {
+    title: "Nuestros Servicios",
+  },
+  contactUs: {},
+};
