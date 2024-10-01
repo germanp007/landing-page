0d4d93a73e7c9560b48a97b670baa182bcdb8b02
germanp007 <pintogerman281@gmail.com>
2024-10-01 18:07:37 -0300

    fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
+import { I18nextProvider } from "react-i18next";
 import { Footer } from "./components/footer/Footer";
 import ResponsiveNav from "./components/navbar/ResponsiveNav";
 import Contact from "./sections/Contact";
 import HelpYou from "./sections/HelpYou";
 import Hero from "./sections/Hero";
 import Services from "./sections/Services";
+import i18n from "./locales/i18n";
 
 const App = () => {
   return (
     <>
-      <ResponsiveNav />
-      <Hero />
-      <HelpYou />
-      <Services />
-      <Contact />
-      <Footer />
+      <I18nextProvider i18n={i18n}>
+        <ResponsiveNav />
+        <Hero />
+        <HelpYou />
+        <Services />
+        <Contact />
+        <Footer />
+      </I18nextProvider>
     </>
   );
 };
--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,13 +1,24 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import { EN } from "./languages/en";
+import { ES } from "./languages/es";
+
+const resources = {
+  en: {
+    translation: EN,
+  },
+  es: {
+    translation: ES,
+  },
+};
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    //
-    fallbackLng: "en",
+    resources,
+    fallbackLng: "es",
     interpolation: {
       escapeValue: false,
     },
