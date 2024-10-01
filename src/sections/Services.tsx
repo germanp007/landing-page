81f224407dd1941a5837ad30862749d9ac59a4cf
germanp007 <pintogerman281@gmail.com>
2024-10-01 15:11:58 -0300

    arreglando bug de services section

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -8,11 +8,11 @@ import { SERVICES_DATA } from "../utils/constants";
 const Services = () => {
   return (
     <>
-      <section className="bg-sectionBg h-[60vh] w-screen flex flex-col overflow-x-hidden space-y-4 py-4 justify-evenly sm700:h-[80vh] md:min-h-screen">
+      <section className="bg-sectionBg w-screen flex flex-col overflow-x-hidden space-y-4 py-4 justify-evenly sm700:min-h-[80vh] ">
         <Titles className="text-white w-full text-center h-auto">
           Nuestros Servicios
         </Titles>
-        <div className="flex overflow-x-auto space-x-4 snap-x snap-mandatory scroll-container gap-4 px-6 lg:grid lg:grid-cols-2 lg:w-[75%vw] lg:h-full lg:m-auto lg:gap-12">
+        <div className="flex overflow-x-auto space-x-4 snap-x snap-mandatory scroll-container gap-4 px-6 lg:grid lg:grid-cols-2 lg:min-h-full lg:m-auto lg:gap-8">
           {SERVICES_DATA.map((ele) => (
             <div className="snap-center shrink-0 m-auto" key={ele.id}>
               <Card element={ele} />
